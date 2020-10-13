d5bb806904880693a38d2b452d3cc25367106b0b
fabio <campos_fabio@hotmail.com>
2020-10-12 21:30:47 -0300

    atualização

--- quizzler/lib/question.dart
+++
@@ -1,9 +0,0 @@
-class Question {
-  String questionText;
-  bool questionAnswer;
-
-  Question(String question, bool answer) {
-    this.questionText = question;
-    this.questionAnswer = answer;
-  }
-}
